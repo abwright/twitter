5c391033545508bb66bb062b0b6d4e3e9c29af22
Andrew Wright <anwright@davidson.edu>
2018-07-24 08:43:10 -0400

    followers/following and more

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,14 @@
 //= require_tree .
 
 
+$(document).ready(function(){
+	$("#unfollow_btn").hover(function(){
+		$(this).removeClass("btn-primary");
+		$(this).addClass("btn-danger");
+		$(this).html("Unfollow");
+	}, function(){
+		$(this).html("Following");
+		$(this).removeClass("btn-danger");
+		$(this).addClass("btn-primary");		
+	});
+})
